36b02274945370ba0da52de090159f68afac361c
Saniya <saniyaanjum142115@gmail.com>
2023-07-29 10:51:50 +0530

    update all files

--- project_first/src/index.js
+++ project_first/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ImgGallery from './Imagegallery';
 // import Header from './stateExample';
-import On from './Onbutton';
+// import On from './Onbutton';
 // import ShowDate from './date';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
